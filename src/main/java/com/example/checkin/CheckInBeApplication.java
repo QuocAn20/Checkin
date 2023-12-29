74f4d140e72f67000c70ab199b7feeddf4c93cfa
Quoc An <quocan17102000@gmail.com>
2023-12-29 09:36:46 +0700

    commit allfile

---
+++ src/main/java/com/example/checkin/CheckInBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/checkin/CheckInBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
